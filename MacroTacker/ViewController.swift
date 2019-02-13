08e9adcb9c4703216355f979907c7727d0f2667f
Trevor Hecht <trevorh12@gmail.com>
2019-02-13 14:25:54 -0800

    Initial Commit

---
+++ MacroTacker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacroTacker
+//
+//  Created by Trevor Hecht on 2/13/19.
+//  Copyright © 2019 Trevor Hecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
