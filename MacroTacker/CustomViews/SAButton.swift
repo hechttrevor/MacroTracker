cb361f14a3aa78e9aefa9e97c2f728edc8285ebe
Trevor Hecht <trevorhecht@Trevors-MacBook-Pro.local>
2019-02-15 12:59:58 -0800

    Second commit, no changes

---
+++ MacroTacker/CustomViews/SAButton.swift
@@ -0,0 +1,31 @@
+//
+//  SAButton.swift
+//  Beginner-Constraints
+//
+//  Created by Sean Allen on 11/29/18.
+//  Copyright © 2018 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class SAButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    
+    private func setupButton() {
+        backgroundColor     = UIColor.blue
+        //titleLabel?.font    = UIFont(name: font.avenirNextCondensedDemiBold, size: 22)
+        layer.cornerRadius  = frame.size.height/2
+        setTitleColor(.white, for: .normal)
+    }
+}
---
+++ Pods/Target Support Files/Pods-MacroTacker/Pods-MacroTacker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MacroTacker : NSObject
+@end
+@implementation PodsDummy_Pods_MacroTacker
+@end
